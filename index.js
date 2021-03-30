f64a64ac1ca600465d194f8285e26cae2dad8f8c
Miquel Castro <castro-miquel@hotmail.com>
2021-03-30 19:16:50 +0200

    si no tengo parametros paro la ejecccion del programa passo numeros a los exit borro el import del process creo la carpeta textos con un ejemplo

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const { exit } = require("process");
 const chalk = require("chalk");
 const guardarSaludo = require("./utilidades/archivos");
 
+if (!process.argv[2]) {
+  process.exit(0);
+}
+
 if (!process.argv[2].includes("--nombre")) {
   console.log(chalk.yellow("No has pasado el parametro nombre"));
-  exit();
+  process.exit(1);
 }
 
 guardarSaludo(process.argv[2].split("=")[1]);
